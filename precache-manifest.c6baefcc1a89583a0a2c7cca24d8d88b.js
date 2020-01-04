977b63359366605b25abec5e8b99a04adc2fe1ec
Oscar Rangel <r4ngel93@debian-DELL>
2020-01-04 01:17:47 -0600

    Updates

---
+++ precache-manifest.c6baefcc1a89583a0a2c7cca24d8d88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f53096bdaf5079ecca7754a8aa6676",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "518a2fc85dcd59b49dc7",
+    "url": "/react-calculator/static/css/main.74dac058.chunk.css"
+  },
+  {
+    "revision": "55e1db70c3e6809865a2",
+    "url": "/react-calculator/static/js/2.f28d433c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.f28d433c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "518a2fc85dcd59b49dc7",
+    "url": "/react-calculator/static/js/main.3f6d0519.chunk.js"
+  },
+  {
+    "revision": "54c0d98f965230fd7a18",
+    "url": "/react-calculator/static/js/runtime-main.f3064f83.js"
+  }
+]);
